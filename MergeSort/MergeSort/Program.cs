503effa61c797d8ec34d60e2a49fccd83a265086
Destosed <luzhbinnikita@gmail.com>
2019-03-03 13:33:48 +0300

    Added merge sort and filling array method

--- MergeSort/MergeSort/Program.cs
+++ MergeSort/MergeSort/Program.cs
@@ -1,15 +1,87 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MergeSort
+namespace MergeSortProj
 {
     class Program
     {
         static void Main(string[] args)
         {
+            GenerateItems();
+            Console.ReadKey();
+        }
+
+        static int[] MergeSort(int[] array)
+        {
+            if (array.Length == 1)
+                return array;
+            int middle = array.Length / 2;
+            return Merge(MergeSort(array.Take(middle).ToArray()), MergeSort(array.Skip(middle).ToArray()));
+        }
+
+        static int[] Merge(int[] arr1, int[] arr2)
+        {
+            int ptr1 = 0, ptr2 = 0;
+            int[] merged = new int[arr1.Length + arr2.Length];
+
+            for (int i = 0; i < merged.Length; ++i)
+            {
+                if (ptr1 < arr1.Length && ptr2 < arr2.Length)
+                    merged[i] = arr1[ptr1] > arr2[ptr2] ? arr2[ptr2++] : arr1[ptr1++];
+                else
+                    merged[i] = ptr2 < arr2.Length ? arr2[ptr2++] : arr1[ptr1++];
+            }
+            return merged;
+        }
+
+        static SingleLinkedList MergeSort(SingleLinkedList lst)
+        {
+            var sortedArr = MergeSort(lst.ToIntArray());
+            var sortedLst = lst.FillFromArray(sortedArr);
+            return sortedLst;
+        }
+
+        static void GenerateItems()
+        {
+            const int setsCount = 3;
+            const int itemsCount = 4 ;
+
+            var itemSetArray = new int[setsCount][];
+
+            FillArray(itemSetArray, itemsCount);
+            PrintArray(itemSetArray);
+        }
+
+        static void FillArray(int[][] itemSetArray, int itemsCount)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < itemSetArray.Length; i++)
+            {
+                itemSetArray[i] = new int[itemsCount];
+
+                for (int j = 0; j < itemSetArray[i].Length; j++)
+                    itemSetArray[i][j] = rnd.Next(0, 100);
+            }
+        }
+
+        static void PrintArray(int[] arr)
+        {
+            foreach (var item in arr)
+                Console.Write("{0} ", item);
+        }
+
+        static void PrintArray(int[][] itemSetArray)
+        {
+            for (int i = 0; i < itemSetArray.Length; i++)
+            {
+                for (int j = 0; j < itemSetArray[i].Length; j++)
+                    Console.Write("{0} ", itemSetArray[i][j]);
+
+                Console.WriteLine();
+            }
         }
     }
 }
---
+++ MergeSort/MergeSort/SingleLinkedList.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeSortProj
+{
+    class Elem
+    {
+        public int Info { get; set; }
+        public Elem Next { get; set; }
+    }
+
+    class SingleLinkedList
+    {
+        public Elem First { get; set; }
+
+        public int Length
+        {
+            get
+            {
+                int k = 0;
+                var el = First;
+                while (el != null)
+                {
+                    k++;
+                    el = el.Next;
+                }
+                return k;
+            }
+        }
+
+        public void AddFirst(int x)
+        {
+            var el = new Elem() { Info = x, Next = First };
+            First = el;
+        }
+
+        public void AddLast(int x)
+        {
+            var el = First;
+            if (el == null)
+            {
+                AddFirst(x);
+                return;
+            }
+            while (el.Next != null)
+                el = el.Next;
+            el.Next = new Elem() { Info = x };
+        }
+
+        public override string ToString()
+        {
+            var el = First;
+            var sb = new StringBuilder();
+            while (el != null)
+            {
+                sb.Append($"{el.Info} ");
+                el = el.Next;
+            }
+            return sb.ToString();
+        }
+
+        public bool IsOrdered()
+        {
+            var el = First;
+            if (el == null || el.Next == null)
+                return true;
+            while (el.Next != null)
+            {
+                if (el.Info > el.Next.Info)
+                    return false;
+                el = el.Next;
+            }
+            return true;
+        }
+
+        public int[] ToIntArray()
+        {
+            var arr = new int[this.Length];
+            var el = First;
+            var k = 0;
+
+            while (el != null)
+            {
+                arr[k] = el.Info;
+                k++;
+                el = el.Next;
+            }
+            return arr;
+        }
+
+        public SingleLinkedList FillFromArray(int[] arr)
+        {
+            var lst = new SingleLinkedList();
+            var el = First;
+            var k = 0;
+            while (el != null)
+            {
+                lst.AddLast(arr[k]);
+                k++;
+                el = el.Next;
+            }
+            return lst;
+        }
+    }
+}
